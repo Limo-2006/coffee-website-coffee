5ee059d55a0cfa598d1cc6857eeae07731fe48bb
Akhi Alam <akhi42218@gmail.com>
2026-01-18 11:01:19 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3, 
+  spaceBetween: 30,
+  loop: true, 
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
